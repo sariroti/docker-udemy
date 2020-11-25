7d947554c0d412d1d916658f52f0d345adbc0d15
Budi Erwanto <budi.erwanto@campaign.com>
2020-11-25 19:45:33 +0700

    Initial Commit

---
+++ simple-web/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('How are you doing');
+});
+
+app.listen(8081, () => {
+  console.log('lisetning on port 8081');
+});
